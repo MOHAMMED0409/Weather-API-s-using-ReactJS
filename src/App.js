caf42fd84b05a00c833d6ad886fefeec9986af45
Mohammed Khashif S <64459922+MOHAMMED0409@users.noreply.github.com>
2024-04-08 12:26:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
-import "./App.css";
+// App.js
+
+import React, { useState, useEffect } from 'react';
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { onAuthUIStateChange } from '@aws-amplify/ui-components';
+import Amplify from './amplifyconfig'; // Import the Amplify configuration
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
-import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
-import { useState } from "react";
 import Forecast from "./components/forecast/forecast";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
-  const [forecast, setforcecast] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    const listener = onAuthUIStateChange((authState, authData) => {
+      console.log(`authState: ${authState}`);
+      console.log(`authData: ${JSON.stringify(authData)}`);
+    });
+    return () => listener();
+  }, []);
+
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
 
-    const currentWeatherFeth = fetch(
-      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&uints=metric`
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
     );
-    const currentforcecastFeth = fetch(
-      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&uints=metric`
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
     );
-    Promise.all([currentWeatherFeth, currentforcecastFeth])
-      .then(async (response) => {
-        const weatherResponse = await response[0].json();
-        const forcecastResponse = await response[1].json();
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (responses) => {
+        const weatherResponse = await responses[0].json();
+        const forecastResponse = await responses[1].json();
 
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setforcecast({ city: searchData.label, ...forcecastResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
       })
       .catch((err) => console.log(err));
   };
 
-  // console.log(currentWeather);
-  console.log(forecast);
-
   return (
-    <div className="container">
-      <Search onSearchChange={handleOnSearchChange} />
-      {currentWeather && <CurrentWeather data={currentWeather} />}
-      {forecast && <Forecast data={forecast} />}
-    </div>
+    <AmplifyAuthenticator>
+      <div className="container">
+        <Search onSearchChange={handleOnSearchChange} />
+        {currentWeather && <CurrentWeather data={currentWeather} />}
+        {forecast && <Forecast data={forecast} />}
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
   );
 }
 
